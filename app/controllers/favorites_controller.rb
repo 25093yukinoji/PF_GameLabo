1c85cb39c620f140e010816d7ef45129750504ba
25093yukinoji <ten.yuki.0601@gmail.com>
2021-03-16 16:55:08 +0000

    Fin favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,26 +1,22 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_variables
-  # 保留
-  # def index
-  #   @favolites = current_user.favorite(@game).all
-  # end
+
+  def index
+    @favorite_games = current_user.favorite_games.page(params[:page]).per(3).reverse_order
+  end
 
   def favorite
+    @game = Game.find(params[:game_id])
+    @id_name = "#favorite-link-#{@game.id}"
     favorite = current_user.favorites.new(game_id: @game.id)
     favorite.save
   end
 
   def unfavorite
-    favorite = current_user.favorites.find_by(game_id: @game.id)
-    favorite.destroy
-  end
-
-  private
-
-  def set_variables
     @game = Game.find(params[:game_id])
     @id_name = "#favorite-link-#{@game.id}"
+    favorite = current_user.favorites.find_by(game_id: @game.id)
+    favorite.destroy
   end
 
 end
