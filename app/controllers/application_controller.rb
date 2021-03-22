728b2dd824a505142a2b84d823eb4b143f4fff89
25093yukinoji <ten.yuki.0601@gmail.com>
2021-03-22 08:18:12 +0000

    All Fin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+# ログイン後の偏移先
   def after_sign_in_path_for(_resource)
     my_page_path
   end
 
   protected
-
+# 名前のカラム追加
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
